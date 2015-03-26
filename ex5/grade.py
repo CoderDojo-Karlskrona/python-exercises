c62a837da69348c2dba914936825dfe848243cfa
pnerg <peter.nerg@gmail.com>
2015-03-26 21:19:43 +0100

    Ex5

---
+++ ex5/grade.py
@@ -0,0 +1,13 @@
+score = float(raw_input("Poang: "))
+if score > 100 or score < 0:
+    print "Score out of range"
+elif score >= 90:
+    print "A"
+elif score >= 80:
+    print "B"
+elif score >= 70:
+    print "C"
+elif score >= 60:
+    print "D"
+else:
+    print "F"
