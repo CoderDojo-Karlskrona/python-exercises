1b97e57e6bdc192d188d98ee7582141d1dd0172c
pnerg <peter.nerg@gmail.com>
2015-03-29 11:47:19 +0200

    ex13

---
+++ ex13/filewriter.py
@@ -0,0 +1,21 @@
+sentences = list()
+
+while True:
+    txt = raw_input("Skriv en mening: ")
+    if txt == 'END':
+        break
+     
+    sentences.append(txt)
+    
+outFile = open('mywords.out', 'w')
+for sentence in sentences:
+    outFile.write(sentence)
+    outFile.write('\n')
+outFile.close()
+
+# emtpy line
+print ""
+
+fh = open("mywords.out")
+for line in fh:
+    print line.rstrip().upper()
