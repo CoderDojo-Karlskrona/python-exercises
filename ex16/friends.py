77e75eda8ba6569a78517feec45f744ce0d33dde
pnerg <peter.nerg@gmail.com>
2015-03-29 14:41:29 +0200

    ex16

---
+++ ex16/friends.py
@@ -0,0 +1,16 @@
+from friendclass import *
+
+friends = list()
+
+while True:
+    name = raw_input("Namn: ")
+    if name == 'END':
+        break
+    number = raw_input("Tfn: ")
+    if number == 'END':
+        break
+     
+    friends.append(Friend(name, number))
+    
+for friend in friends:
+    print friend.toString()
